7d72b4605868b2139efcb6244f7dbcc927d328f3
Noa Flaherty <noaflaherty@gmail.com>
2018-11-04 10:47:10 -0500

    Tweak procfile

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,4 @@ from server import app as application
 import os
 
 if __name__ == '__main__':
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    os.environ['LOCATION'] = 'PROD'
     application.run()
